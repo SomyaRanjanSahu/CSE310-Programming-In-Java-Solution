c1ea2a6b3ab7973e2630316a68a6caeb4231bf97
Somya_Sahu <somyasahu10@gmail.com>
2022-02-28 00:50:30 +0530

    Updated Question 07

--- Lecture 34/Question07.java
+++ Lecture 34/Question07.java
@@ -39,3 +39,25 @@ class ICICI extends Bank{
 		return (p*INTEREST_RATE*t)/100;
 	}
 }
+
+class AXIS extends Bank{
+	private static final float INTEREST_RATE = 12.3f;
+	
+	float calculateInterest(float p, int t){
+		return (p*INTEREST_RATE*t)/100;
+	}
+}
+
+public class TestOverriding {
+	public static void main(String[] args) {
+		Bank sbiBank = new SBI();
+		Bank  iciciBank = new ICICI();
+		Bank axisBank = new AXIS();
+		float principal = Float.parseFloat(args[0]);
+		int time = Integer.parseInt(args[1]);
+		System.out.println("SBI rate of interest = " + sbiBank.calculateInterest(principal,time));
+		System.out.println("ICICI rate of interest = " + iciciBank.calculateInterest(principal, time));
+		System.out.println("AXIS rate of interest = " + axisBank.calculateInterest(principal, time));
+	}
+}
+
