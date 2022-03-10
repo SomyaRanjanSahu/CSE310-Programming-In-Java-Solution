a646462304a4d96dd6b9bf11cc55be4aef18305e
Somya_Sahu <somyasahu10@gmail.com>
2022-03-10 17:52:29 +0530

    Added Lecture 57 to 62

---
+++ Lecture 57/Question07.java
@@ -0,0 +1,22 @@
+/* 
+
+Write a program to understand how to retrive an element in an ArrayList using the method get.
+
+Create a class ListDemo with a main method and get the element at index 1.
+
+Write the missing code in the below program.
+
+*/
+
+package q11958;
+import java.util.*;
+public class ListDemo {
+	public static void main(String[] args) {
+		List<String> namesList = new ArrayList<String>();
+		namesList.add("Mercury");
+		namesList.add("Venus");
+		namesList.add("Earth");
+		namesList.add("Mars");
+		System.out.println(namesList.get(1)); // write your logic here
+	}
+}
---
+++ Lecture 61/Question05.java
@@ -0,0 +1,24 @@
+/* 
+
+Below code shows how to use generics while using a Map. Observe how we iterate only keys of the Map.
+
+Note: Please don't change the package name.
+
+*/
+
+package q11392;
+import java.util.*;
+public class SimpleMapDemo {
+	public static void main(String[] args) {
+		Map<String, String> countryCodesMap = new HashMap<String, String>();
+		countryCodesMap.put("IN", "India");
+		countryCodesMap.put("CA", "Canada");
+		countryCodesMap.put("AG", "Argentina");
+		countryCodesMap.put("BR", "Brazil");
+		Set<String> codesSet = countryCodesMap.keySet();
+		for (String code : codesSet) {
+			String countryName = countryCodesMap.get(code);
+			System.out.println(code + " is the code for : " + countryName);
+		}
+	}
+}
